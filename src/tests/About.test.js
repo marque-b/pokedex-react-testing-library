fb27c01ab98515dd320aaa6aa4725ca528fb8607
Bruno Marques <brunorcmarques@gmail.com>
2022-08-16 18:08:05 -0300

    Req 2 - Testa componente About

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,35 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import renderWithRouter from './renderWithRouter';
+import About from '../pages/About';
+
+describe('Teste do componente About', () => {
+  it('O título da página é exibido', () => {
+    renderWithRouter(<About />);
+
+    const pageTitle = screen
+      .getByRole('heading', { level: 2, name: /About Pokédex/i });
+
+    expect(pageTitle).toBeInTheDocument();
+  });
+
+  it('São exibidos dois parágrafos com informações', () => {
+    renderWithRouter(<About />);
+
+    const paragraph1 = screen.getByText(/This application simulates a Pokédex, a */i);
+    const paragraph2 = screen.getByText(/One can filter Pokémons by type, and see */i);
+
+    expect(paragraph1).toBeInTheDocument();
+    expect(paragraph2).toBeInTheDocument();
+  });
+
+  it('É exibida uma imagem correta da Pokédex', () => {
+    renderWithRouter(<About />);
+
+    const correctImgSrc = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+
+    const pokedexImg = screen.getByRole('img', { name: /Pokédex/i });
+
+    expect(pokedexImg.src).toBe(correctImgSrc);
+  });
+});
