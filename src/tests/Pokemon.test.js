ef14fe448008bf36d4e80bb84855f2a36bcce1dc
Bruno Marques <brunorcmarques@gmail.com>
2022-08-17 17:56:36 -0300

    Req 6 - Testa o componente Pokemon

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
 
@@ -20,8 +21,30 @@ describe('Testa o componente Pokemon', () => {
     expect(pokemonImg).toBeInTheDocument();
   });
 
-  it('O card do pokémon exibe um link de navegação "More details"', () => {
-    renderWithRouter();
+  it('O card do pokémon exibe um link de navegação "More details"', async () => {
+    const { history } = renderWithRouter(<App />);
 
-  })
+    const moreDetailsLink = screen.getByRole('link', { name: /More details/i });
+    expect(moreDetailsLink).toBeInTheDocument();
+
+    userEvent.click(moreDetailsLink);
+    expect(history.location.pathname).toBe('/pokemons/25');
+  });
+
+  it('É exibido o ícone de estrela na página de favoritos', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const moreDetailsLink = screen.getByRole('link', { name: /More details/i });
+    userEvent.click(moreDetailsLink);
+    const favorite = screen.getByRole('checkbox', { name: /Pokémon favoritado?/i });
+    userEvent.click(favorite);
+
+    history.push('/favorites');
+
+    const pokemonImg = screen.getByRole('img', { name: /Pikachu sprite/i });
+    const starIcon = screen.getByRole('img', { name: /Pikachu is marked as favorite/i });
+    expect(pokemonImg.src).toContain('https://cdn2.bulbagarden.net/');
+    expect(starIcon).toBeInTheDocument();
+    expect(starIcon.src).toContain('/star-icon.svg');
+  });
 });
